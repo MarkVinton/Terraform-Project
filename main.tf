d2207582b045d45c572c4f885ad10c1ac178c6b7
MarkVinton <mark.vinton@aol.com>
2024-03-04 10:41:30 +0000

    Feat: Terraform code for VPC

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source = "./modules/vpc"
+  cidr_block = var.cidr_block
+  public_subnets_cidr = var.public_subnets_cidr
+  availability_zones = var.availability_zones
+  private_subnets_cidr = var.private_subnets_cidr
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "cidr_block" {
+  
+}
+variable "public_subnets_cidr" {
+  
+}
+variable "availability_zones" {
+  
+}
+variable "private_subnets_cidr" {
+  
+}
