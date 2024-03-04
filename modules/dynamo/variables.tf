772eb1634e20eb1f5ef3ea4c182e26974cee51c9
MarkVinton <mark.vinton@aol.com>
2024-03-04 14:16:17 +0000

    Feat: Added dynamo module and created 2 tables with module

---
+++ modules/dynamo/variables.tf
@@ -0,0 +1,9 @@
+variable "db_table_name" {
+  description = "Name of dynamo table"
+}
+variable "hash_key_name" {
+  description = "Name of hash key"
+}
+variable "hash_key_type" {
+  description = "Type of hash key"
+}
