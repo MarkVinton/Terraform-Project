f5d8b3a1264630e5a7808a121bc1ced455fa1158
MarkVinton <mark.vinton@aol.com>
2024-03-04 10:53:36 +0000

    Feat: Added descriptions for variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,16 @@
 variable "cidr_block" {
-  
-}
-variable "public_subnets_cidr" {
+  description = "CIDR for VPC"
   
 }
 variable "availability_zones" {
-  
+  description = "A list of availability zones"
+
+}
+variable "public_subnets_cidr" {
+  description = "A list of CIDR for public subnets"
+
 }
 variable "private_subnets_cidr" {
+  description = "A list of CIDR for private subnets"
   
 }
